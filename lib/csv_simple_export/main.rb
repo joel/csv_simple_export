47a8dc49ed4395bcf7aea6202c108ef4504fc511
Joel Azemar <joel.azemar@gmail.com>
2021-05-25 15:29:32 +0200

    Rubocop

--- lib/csv_simple_export/main.rb
+++ lib/csv_simple_export/main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CsvSimpleExport
   class Main
     def csv_simple_export(input)
--- spec/csv_simple_export/main_spec.rb
+++ spec/csv_simple_export/main_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module CsvSimpleExport
   RSpec.describe Main do
-    describe '#csv_simple_export' do
-      context 'Example 1' do
-        let(:input) { 'foo' }
-        let(:result) { 'foo' }
+    describe "#csv_simple_export" do
+      context "Example 1" do
+        let(:input) { "foo" }
+        let(:result) { "foo" }
         it do
           expect(subject.csv_simple_export(input)).to eql(result)
         end
